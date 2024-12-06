26a97a0efb23741ebaab6c7a44c17499b9e76870
hlopzz <harid.lopez@gmail.com>
2024-12-06 14:28:32 -0400

    Add files via upload

---
+++ Day5FileProyect.py
@@ -0,0 +1,89 @@
+import random
+
+# Soldier
+
+
+class Soldier:
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    def attack(self):
+        return self.strength
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+   
+
+# Viking
+
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength)
+        self.name = name
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"{self.name} has received {damage} points of damage"
+        else:
+            return f"{self.name} has died in act of combat"
+
+# Saxon
+
+class Saxon(Soldier):
+    def __init__(self, health, strength):
+        super().__init__(health, strength)
+
+    def receiveDamage(self, damage):
+        self.health -= damage
+        if self.health > 0:
+            return f"A Saxon has received {damage} points of damage"
+        else:
+            return "A Saxon has died in combat"
+
+# Davicente
+
+class War:
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+
+    def vikingAttack(self):
+        if not self.saxonArmy:
+            return None
+        saxon = self.saxonArmy[0]
+        viking = self.vikingArmy[0]
+        result = saxon.receiveDamage(viking.strength)
+        if saxon.health <= 0:
+            self.saxonArmy.remove(saxon)
+        return result
+
+    def saxonAttack(self):
+        if not self.vikingArmy:
+            return None
+        viking = self.vikingArmy[0]
+        saxon = self.saxonArmy[0]
+        result = viking.receiveDamage(saxon.strength)
+        if viking.health <= 0:
+            self.vikingArmy.remove(viking)
+        return result
+
+    def showStatus(self):
+        if not self.saxonArmy:
+            return "Vikings have won the war of the century!"
+        elif not self.vikingArmy:
+            return "Saxons have fought for their lives and survive another day..."
+        else:
+            return "Vikings and Saxons are still in the thick of battle."
+
+
