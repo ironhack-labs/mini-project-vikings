68fe35c8ce2d20b6594fda3f996db76e42c15422
RJavierr <00rgh1@salud.gov.pr>
2024-12-15 12:04:22 -0400

    Week 1 Mini Project 1

--- wargame.py
+++ wargame.py
@@ -1,27 +1,32 @@
-# With a correction already implemented: dont forget to initialize an instance of Class "War"
-
-
-from .vikingsClasses import Soldier, Viking, Saxon, War
+from vikingsClasses import Soldier, Viking, Saxon, War
 import random
-
 
 soldier_names = ["albert","andres","archie","dani", "david","gerard","german","graham","imanol","laura"]
 great_war = War()
 
-#Create 5 Vikings
-for i in range(0,5):
-    if i:
-        great_war.addViking(Viking(soldier_names[random.randint(0,9)],100,random.randint(0,100)))
+# Crear 5 Vikings con nombres aleatorios
+for _ in range(5):
+    name = random.choice(soldier_names)
+    health = 100
+    strength = random.randint(50, 100)
+    great_war.addViking(Viking(name, health, strength))
 
-#Create 5 Saxons
-for i in range(0,5):
-    if i:
-        great_war.addSaxon(Saxon(100,random.randint(0,100)))
-    
+# Crear 5 Saxons con atributos aleatorios
+for _ in range(5):
+    health = 100
+    strength = random.randint(50, 100)
+    great_war.addSaxon(Saxon(health, strength))
+
+# Simular la guerra
 round = 0
 while great_war.showStatus() == "Vikings and Saxons are still in the thick of battle.":
+    # Realizar ataques
     great_war.vikingAttack()
     great_war.saxonAttack()
-    print(f"round: {round} // Viking army: {len(great_war.vikingArmy)} warriors",f"and Saxon army: {len(great_war.saxonArmy)} warriors")
+    
+    # Mostrar el estado de cada ronda
+    print(f"round: {round} // Viking army: {len(great_war.vikingArmy)} warriors",
+          f"and Saxon army: {len(great_war.saxonArmy)} warriors")
     print(great_war.showStatus())
+    
     round += 1
